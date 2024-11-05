047b5544cc8e98f554ac7fc5490d27801af98fbd
Soo <parksk971031@gmail.com>
2024-11-05 13:10:09 -0500

    complete landing section on hero page

--- startup-bank/app/(root)/page.tsx
+++ startup-bank/app/(root)/page.tsx
@@ -3,9 +3,18 @@
 export default function Home () {
   return(
     <>
-    <h1 className="heading">
-      HI THERE
-    </h1>
+    <section className="pink_container">
+      <h2 className="tag">
+        Pitch, Vote, and Grow
+      </h2>
+      <h1 className="heading">
+      Pitch Your Startup,<br/>Connect with Entrepreneurs
+      </h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+      </p>
+    </section>
+    
     </>
   )
 }
