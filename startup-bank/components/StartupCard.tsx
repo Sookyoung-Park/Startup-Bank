0312e178bb0b9c2ae52f5cb127eea590992382c9
Soo <parksk971031@gmail.com>
2024-11-06 18:31:05 -0500

    compelte frontend startupcard section

--- startup-bank/components/StartupCard.tsx
+++ startup-bank/components/StartupCard.tsx
@@ -2,37 +2,76 @@ import React from 'react'
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from './ui/button'
 
 const StartupCard = ({post}:{post: StartupTypeCard}) => {
-  return (
-    <li className='startup-card group'>
-        <div className="flex-between">
-            <p className="startup-card_date">
-                {formatDate(post._createdAt)}
-            </p>
-            <div className='flex gap-1.5'>
-                <EyeIcon className='size-6 text-primary-100'/>
-                <span className='text-16-medium'>{post.views}</span>
-            </div>
-        </div>
-        <div className="flex-between mt-5 gap-5">
-            <div className='flex-1'>
-                <Link href={`/users/{post.author?._id}`}>
-                    <p className="text-16-medium line-clamp-1">
-                        {post.author?.name}
-                    </p>
-                </Link>
-                <p>
-                    {post.title}
+    const {_createdAt, views, author:{_id:authorId, name},description, title, category, _id, image } = post
+
+    return (
+        <li className='startup-card group'>
+            <div className="flex-between">
+                <p className="startup-card_date">
+                    {formatDate(_createdAt)}
                 </p>
+                <div className='flex gap-1.5'>
+                    <EyeIcon className='size-6 text-primary'/>
+                    <span className='text-16-medium'>{views}</span>
+                </div>
+            </div>
+            <div className="flex-between mt-5 gap-5">
+                <div className='flex-1'>
+                    <Link href={`/users/${authorId}`}>
+                        <p className="text-16-medium line-clamp-1">
+                            {name}
+                        </p>
+                    </Link>
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className="text-26-semibold line-clamp-1">
+                            {title}
+                        </h3>
+                    </Link>
+                </div>
+
+                <Link href={`/users/${authorId}`}>
+                    <Image
+                        src="https://placehold.co/48x48"
+                        alt="authorProfile"
+                        width={48}
+                        height={48} 
+                        className='rounded-full'
+                    />
+                </Link>
+            </div>
+
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card-desc">
+                    {description}
+                </p>
+                <Image 
+                    src={image}
+                    className='startup-card_img'
+                    alt="postImage"
+                    width={276}
+                    height={164}
+                    />
+            </Link>
+            
+            <div className="flex-between gap-3 mt-5">
+                <Link
+                    href={`/?query=${category.toLowerCase()}`}>
+                        <p className="text-16-medium">{category}</p>
+                </Link>
+                <Button className='startup-card_btn' asChild>
+                    <Link href={`/startup/${_id}`}>
+                        Details
+                    </Link>
+
+                </Button>
             </div>
             
-            
-            
-        </div>
-        
-    </li>
-  )
+        </li>
+    )
 }
 
 export default StartupCard
