888d8124f4c18d56ccfc4927e09bcab497c538e6
Soo <parksk971031@gmail.com>
2024-11-09 19:25:10 -0500

    ongoing

--- startup-bank/sanity/lib/client.ts
+++ startup-bank/sanity/lib/client.ts
@@ -6,5 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  // setting for always users get a new data(contents)
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
