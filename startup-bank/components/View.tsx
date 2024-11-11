89d946261bad2ad3b122d89f6009df51a6d1601a
Soo <parksk971031@gmail.com>
2024-11-11 16:34:33 -0500

    implementation ongoing detialed startup page

--- startup-bank/components/View.tsx
+++ startup-bank/components/View.tsx
@@ -1,14 +1,21 @@
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { client } from '@/sanity/lib/client'
 import React from 'react'
 import { Ping } from './Ping'
 
-const View = ({id}:{id:string}) => {
+const View = async ({id}:{id:string}) => {
+  
+  const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY,{id})
+
+// update the number of views
+
   return (
     <div className="view-container">
         <div className="absolute -top-2 -right-2">
             <Ping/>
         </div>
         <p className="view-text">
-            <span className='font-black'>100 views</span>
+            <span className='font-black'>{totalViews} views</span>
         </p>
     </div>
   )
