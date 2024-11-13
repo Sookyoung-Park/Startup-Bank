30242ee0585707a25225cef2ad53376fb69e6524
Soo <parksk971031@gmail.com>
2024-11-12 20:36:22 -0500

    complete implemeting frontend create page

--- startup-bank/components/StartupForm.tsx
+++ startup-bank/components/StartupForm.tsx
@@ -1,11 +1,15 @@
 "use client"
 import React, { useState } from 'react'
 import {Input} from '@/components/ui/input'
-import { errorToJSON } from 'next/dist/server/render'
 import { Textarea } from './ui/textarea'
+import MDEditor from '@uiw/react-md-editor'
+import { Button } from './ui/button'
+import { Send } from 'lucide-react'
 
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string,string>>({})
+    const [pitch, setPitch] = useState('')
+    const isPending = false
     
     return (
         <form 
@@ -54,6 +58,33 @@ const StartupForm = () => {
                 />
                 {errors.link && <p className='startup-form_error'>{errors.link}</p>}
             </div>
+
+            <div data-color-mode="light">
+                <label htmlFor="pitch" className='startup-form_label'>
+                    Pitch
+                </label>
+                <MDEditor
+                    value={pitch}
+                    onChange={(value)=>setPitch(value as string)}
+                    id="pitch"
+                    preview="edit"
+                    height={300}
+                    style={{borderRadius:20, overflow:"hidden"}}
+                    textareaProps={{
+                        placeholder:"Briefly describe your idea and what problem it solves"
+                    }}
+                    previewOptions={{
+                        disallowedElements:["style"],
+                    }}
+                /> 
+                {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
+            </div>
+
+            <Button type="submit" className='startup-form_btn text-white'
+            disabled={isPending}>
+                {isPending ? 'Submitting...' : 'Submit your Pitch '}
+                <Send className='size-6 ml-2'/>
+            </Button>
         
         </form>
     )
