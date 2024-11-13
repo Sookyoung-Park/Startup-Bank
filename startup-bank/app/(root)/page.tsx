163447bd8a6d79a3edf8337f6c954e7d12ed8311
Soo <parksk971031@gmail.com>
2024-11-12 19:54:09 -0500

    implement auth caching

--- startup-bank/app/(root)/page.tsx
+++ startup-bank/app/(root)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home ({searchParams}:{
   const query= (await searchParams).query
   const params = {search: query || null}
   const session = await auth()
-  console.log('here:',session.id)
+  console.log('here:',session?.id)
 
   const {data:posts} = await sanityFetch({query:STARTUPS_QUERY, params})
   
--- startup-bank/auth.ts
+++ startup-bank/auth.ts
@@ -1,40 +1,93 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import { client } from "./sanity/lib/client"
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries"
-import { writeClient } from "./sanity/lib/write-client"
- 
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({user:{name, email, image}, profile:{id, login, bio}}){
-      const  existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-        id: profile?.id,
-      })
-      if(!existingUser){
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio },
+    }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+
+      if (!existingUser) {
         await writeClient.create({
-          _type:'author',
+          _type: "author",
           id,
           name,
           username: login,
           email,
           image,
-          bio: bio||''
-        })
+          bio: bio || "",
+        });
       }
-      return true
+
+      return true;
     },
-    async jwt({token, account, profile}){
-      if(account && profile){
-        const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id})
-        token.id = user._id
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
       }
-      return token
+
+      return token;
     },
-    async session({token, session}){
-      Object.assign(session,{id: token.id})
-      return session
-    }
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
   },
-})
+});
+
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import { client } from "./sanity/lib/client"
+// import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries"
+// import { writeClient } from "./sanity/lib/write-client"
+ 
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [GitHub],
+//   callbacks: {
+//     async signIn({user:{name, email, image}, profile:{id, login, bio}}){
+//       const  existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+//         id: profile?.id,
+//       })
+//       if(!existingUser){
+//         await writeClient.create({
+//           _type:'author',
+//           id,
+//           name,
+//           username: login,
+//           email,
+//           image,
+//           bio: bio||''
+//         })
+//       }
+//       return true
+//     },
+//     async jwt({token, account, profile}){
+//       if(account && profile){
+//         const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id})
+//         token.id = user?._id
+//       }
+//       return token
+//     },
+//     async session({token, session}){
+//       Object.assign(session,{id: token.id})
+//       return session
+//     }
+//   },
+// })
 
