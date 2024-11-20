b518e78ae306702db8c753ab5d7f62658354a77c
Soo <parksk971031@gmail.com>
2024-11-19 19:45:52 -0500

    complete user profile

--- startup-bank/app/(root)/user/[id]/page.tsx
+++ startup-bank/app/(root)/user/[id]/page.tsx
@@ -3,6 +3,11 @@ import { client } from '@/sanity/lib/client'
 import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries'
 import { notFound } from 'next/navigation'
 import React from 'react'
+import Image from 'next/image'
+import UserStartups, { StartupCardSkeleton } from '@/components/UserStartups'
+import { Suspense } from 'react'
+
+export const experimental_ppr= true
 
 const page = async ({params}: {params:Promise<{id:string}>}) => {
     const id = (await params).id
@@ -24,7 +29,31 @@ const page = async ({params}: {params:Promise<{id:string}>}) => {
                         {user.name}
                     </h3>
                 </div>
-            </div>
+                    <Image
+                        src={user.image}
+                        alt={user.name}
+                        width={220}
+                        height={220}
+                        className="profile_image"
+                    />
+                    <p className="text-30-extrabold mt-7 text-center">
+                        @{user?.username}
+                    </p>
+                    <p className="mt-1 text-center text-14-normal">
+                        {user?.bio}
+                    </p>
+                </div>
+
+                <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+                    <p className="text-30-bold">
+                        {session?.id === id ? "Your" : "All"} Startups
+                    </p>
+                    <ul className="card_grid-sm">
+                        <Suspense fallback={<StartupCardSkeleton/>}>
+                            <UserStartups id={id}/>
+                        </Suspense>
+                    </ul>
+                </div>
         </section>
     </>
   )
